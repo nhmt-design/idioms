c56bcc3dffe856e40cb3a4224c758e03a5c85db3
nhmt-design <nhmt@nhhs.edu.sg>
2026-07-23 15:32:25 +0800

    Delete netlify directory

--- netlify/functions/lib/config.mjs
+++
@@ -1,7 +0,0 @@
-const required = ["SUPABASE_URL", "SUPABASE_SERVICE_ROLE_KEY", "SESSION_SECRET", "LOGIN_CODE_PEPPER", "TEACHER_PASSWORD"];
-
-export const config = () => {
-  const missing = required.filter((key) => !process.env[key]);
-  if (missing.length) throw new Error(`Missing environment variables: ${missing.join(", ")}`);
-  return Object.fromEntries(required.map((key) => [key, process.env[key]]));
-};
